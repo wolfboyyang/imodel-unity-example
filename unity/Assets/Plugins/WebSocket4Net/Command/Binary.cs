7670a283f52fb27a7ca1cc7e3c7d97539d100d9d
Wei Yang <wei.yang@rwth-aachen.de>
2022-05-08 20:38:13 +0200

    Change dll to source files.

---
+++ unity/Assets/Plugins/WebSocket4Net/Command/Binary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebSocket4Net.Command
+{
+    public class Binary : WebSocketCommandBase
+    {
+        public override void ExecuteCommand(WebSocket session, WebSocketCommandInfo commandInfo)
+        {
+            session.FireDataReceived(commandInfo.Data);
+        }
+
+        public override string Name
+        {
+            get { return OpCode.Binary.ToString(); }
+        }
+    }
+}
